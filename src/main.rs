4b759489cc7e6eba88cd10dfef94d51f77137dc3
Tudor <uiv80988@vitesco.com>
2026-05-11 14:49:01 +0300

    added bias addition

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,54 @@
 use rand::Rng;
 
 
+fn prod(weights: Vec<Vec<f32>>, input: Vec<f32>) -> Vec<f32>{
+
+	let mut output= vec![];	
+
+
+	for i in 0..weights.len() {
+		let mut sum :f32 = 0.0;
+		for j in 0..weights[0].len(){	
+
+			sum += weights[i][j] * input[j];
+
+		}
+		output.push(sum);
+		}
+
+	output
+
+}
+
+
+
+
+
+
+fn add(input: Vec<f32>,bias: Vec<f32>) -> Vec<f32>{
+
+	let mut output = vec![];
+
+
+	for i in 0..input.len(){
+
+		output.push(input[i] + bias[0]);
+
+}
+
+
+
+	output
+
+
+
+}
+
+
+
+
+
+
 struct Layer{
 	activations: Vec<f32>,
 }
@@ -12,14 +60,22 @@ impl Layer {
 			activations : input_data,
 
 		}
+}
+	
+	fn new(input: Layer, cons: Connections ) -> Self{
+
+	Self{
+		activations: vec![],//placeholder for now
+	
+	}	
 
 
 
 }
 
 
-
 }
+
 
 
 
@@ -30,6 +86,8 @@ struct Connections{
 }
 
 
+
+
 impl Connections{
 
 	fn new(st_layer_len: usize,nd_layer_len: usize) -> Self {
@@ -37,21 +95,18 @@ impl Connections{
 		
 		let mut weights = 
 		(0..st_layer_len).map(|_| {
-		(0..nd_layer_len).map(|_| rng.get::<f64>() * 4.0 - 2.0)//random in (-2,2)
+		(0..nd_layer_len).map(|_| rng.gen::<f32>() * 4.0 - 2.0)//random in (-2,2)
 		.collect()}).collect();
 		
 		let mut biases = 
-		(0..nd_later_len).map(|_| rng.get::<f64>() * 4.0 - 2.0) //hope the biases don't have too big a range
+		(0..nd_layer_len).map(|_| rng.gen::<f32>() * 4.0 - 2.0) //hope the biases don't have too big a range
 		.collect();
 
 	Self {
 		weights: weights,
-
-
+		biases: biases,
 	}
 }
-
-
 
 }
 
